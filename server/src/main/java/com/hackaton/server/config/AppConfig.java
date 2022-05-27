d7648592d93c0d2e86a4c48a23392aba30d8dede
Ryan Christilaw <ryan.christilaw@gmail.com>
2022-05-27 11:01:46 -0400

    Set default timezone

--- server/src/main/java/com/hackaton/server/config/AppConfig.java
+++ server/src/main/java/com/hackaton/server/config/AppConfig.java
@@ -3,7 +3,15 @@ package com.hackaton.server.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @Configuration
 @EnableScheduling
 public class AppConfig {
+
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
