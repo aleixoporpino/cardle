b5d07d8960b1b0df49e8c330fcc57be1335251ac
Jose Aleixo Araujo Porpino Filho <aleixoporpino@gmail.com>
2022-05-30 09:32:30 -0400

    Change deploy api url

--- web/src/api/api.js
+++ web/src/api/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://cardle-server-nau7apa67q-uc.a.run.app/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
